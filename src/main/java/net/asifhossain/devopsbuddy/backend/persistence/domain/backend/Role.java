f1e91f5e4a73feee2b406f50419295b531a2dcd3
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2018-05-06 16:57:20 +0600

    Add jpa entities for spring security

---
+++ src/main/java/net/asifhossain/devopsbuddy/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,62 @@
+package net.asifhossain.devopsbuddy.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+
+    private Set<UserRole> userRoles;
+
+    public Role() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
